78fa002b043be3b336498c047a8ffa2d1733200d
jwx <904653684@qq.com>
2019-03-09 21:47:03 +0800

    first step

---
+++ code/load_video3.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('test.avi')
+# 定义fourecc代码
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+
+all_images = []
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret is True:
+        all_images.append(frame)
+        cv2.imshow('camera', frame)
+        if cv2.waitKey(1) & 0xFF is ord('q'):
+            break
+    else:
+        break
+
+sample_img = all_images[0]
+size = (sample_img.shape[1], sample_img.shape[0])
+# 创建VideoWriter对象
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, size)
+
+for img in all_images:
+    out.write(img)
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
