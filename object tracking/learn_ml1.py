39e0f2881b630bc2f9b2b4df92325be8564d36ae
jwx <904653684@qq.com>
2019-04-26 20:04:28 +0800

    wow, some change

--- object tracking/learn_ml1.py
+++
@@ -1,5 +0,0 @@
-import cv2
-
-model = cv2.ml.ANN_MLP_create()
-model.setActivationFunction(cv2.ml.ANN_MLP_SIGMOID_SYM)
-model.setLayerSizes(np.int32([38400, 32, 4]))
--- object tracking/try1.py
+++ object tracking/try1.py
@@ -11,85 +11,85 @@ out = cv2.VideoWriter('output1.avi', fourcc, 25.0, (800, 1422))
 # construct the argument parser and parse the argument
 ap = argparse.ArgumentParser()
 ap.add_argument('-v', '--video', type = str, \
-	help = 'path to input file')
+    help = 'path to input file')
 ap.add_argument('-t', '--tracker', type = str, \
-	default = 'kcf', help = 'OpenCV object tracker type')
+    default = 'kcf', help = 'OpenCV object tracker type')
 args = vars(ap.parse_args())
 
 (major, minor) = cv2.__version__.split('.')[:2]
 
 if int(major) == 3 and int(minor) < 3:
-	tracker = cv2.Tracker_create(args['tracker'].upper())
+    tracker = cv2.Tracker_create(args['tracker'].upper())
 else:
-	OPENCV_OBJECT_TRACKERS = {
-		"csrt": cv2.TrackerCSRT_create,
-		"kcf": cv2.TrackerKCF_create,
-		"boosting": cv2.TrackerBoosting_create,
-		"mil": cv2.TrackerMIL_create,
-		"tld": cv2.TrackerTLD_create,
-		"medianflow": cv2.TrackerMedianFlow_create,
-		"mosse": cv2.TrackerMOSSE_create
-	}
-	tracker = OPENCV_OBJECT_TRACKERS[args['tracker']]()
+    OPENCV_OBJECT_TRACKERS = {
+        "csrt": cv2.TrackerCSRT_create,
+        "kcf": cv2.TrackerKCF_create,
+        "boosting": cv2.TrackerBoosting_create,
+        "mil": cv2.TrackerMIL_create,
+        "tld": cv2.TrackerTLD_create,
+        "medianflow": cv2.TrackerMedianFlow_create,
+        "mosse": cv2.TrackerMOSSE_create
+    }
+    tracker = OPENCV_OBJECT_TRACKERS[args['tracker']]()
 
 # initialize the bounding box coordinates of the object we are going to track
 initBB = None
 
 if not args.get('video', False):
-	print('[info] starting video stream...')
-	vs = VideoStream(src = 0).start()
-	time.sleep(1.0)
+    print('[info] starting video stream...')
+    vs = VideoStream(src = 0).start()
+    time.sleep(1.0)
 else:
-	vs = cv2.VideoCapture(args['video'])
+    vs = cv2.VideoCapture(args['video'])
 
 fps = None
 
 i = 0
 
 while True:
-	i = i + 1
-	frame = vs.read()
-	frame = frame[1] if args.get('video', False) else frame
-	if frame is None:
-		break
-	if i is 3:
-		i = 0
-		frame = imutils.resize(frame, width = 800)
-		(H, W) = frame.shape[:2]
+    frame = vs.read()
+    frame = frame[1] if args.get('video', False) else frame
+    if frame is None:
+        break
+    i = i + 1
+    if i is 3:
+        i = 0
+        frame = imutils.resize(frame, width = 800)
+        (H, W) = frame.shape[:2]
 
-		if initBB is not None:
-			(success, box) = tracker.update(frame)
-			if success:
-				(x, y, w, h) = [int(v) for v in box]
-				cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-			fps.update()
-			fps.stop()
+        if initBB is not None:
+            (success, box) = tracker.update(frame)
+            if success:
+                (x, y, w, h) = [int(v) for v in box]
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            fps.update()
+            fps.stop()
 
-			info = [
-				('tracker', args['tracker']),
-				('success', 'yes' if success else 'no'),
-				('fps', '{:.2f}'.format(fps.fps())),]
-			for (i, (k, v)) in enumerate(info):
-				text = "{}: {}".format(k, v)
-				cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
-					cv2.FONT_HERSHEY_SIMPLEX,  0.6, (0, 0, 255), 2)
-		cv2.imshow('frame', frame)
-		out.write(frame)
-		key = cv2.waitKey(1) & 0xFF
+            info = [
+                ('tracker', args['tracker']),
+                ('success', 'yes' if success else 'no'),
+                ('fps', '{:.2f}'.format(fps.fps())),]
+            for (i, (k, v)) in enumerate(info):
+                text = "{}: {}".format(k, v)
+                cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+                    cv2.FONT_HERSHEY_SIMPLEX,  0.6, (0, 0, 255), 2)
+        cv2.imshow('frame', frame)
+        out.write(frame)
+        key = cv2.waitKey(1) & 0xFF
 
-		if key == ord('s'):
-			initBB = cv2.selectROI('frame', frame, fromCenter = False, showCrosshair = True)
-			tracker.init(frame, initBB)
-			fps = FPS().start()
-		elif key == ord('q'):
-			break
+        if key == ord('s'):
+            initBB = cv2.selectROI('frame', frame, fromCenter = False, showCrosshair = True)
+            tracker.init(frame, initBB)
+            fps = FPS().start()
+        elif key == ord('q'):
+            break
 
 
 if not args.get('video', False):
-	vs.stop()
+    vs.stop()
 
 else:
-	vs.release()
+    vs.release()
 
 
 out.release()
